2d079a8b32ac8c01b025ce5b5f6c96c5797bcbad
warada0209 <87284458+warada0209@users.noreply.github.com>
2021-07-15 08:21:11 +0900

    refactor: A code change that neither fixes a bug nor adds a feature

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pycalil",
-    version="1.0",
+    version="1.2",
     author="warada0209",
     author_email="warada0209@gmail.com",
     description="A package that enables the Calil Library API to be used from Python.",
