b2adb9940fdd1649957606e0b665c8c53a2bbee8
warada <87284458+warada0209@users.noreply.github.com>
2021-07-20 14:03:21 +0900

    fix: A bug fix

--- pycalil/pycalil.py
+++ pycalil/pycalil.py
@@ -7,12 +7,13 @@ This software is released under the MIT License, see LICENSE.
 # https://calil.jp/doc/api_ref.html
 import requests
 
+
 class Pycalil(object):
 
     def __init__(self, apikey):
         self.apikey = apikey
 
-    def library(self, pref=None, city=None, systemid=None, geocode=None, format="json", callback="json", limit=None):
+    def library(self, pref=None, city=None, systemid=None, geocode=None, format="json", callback=" ", limit=None):
         """
         指定した条件で図書館の一覧を取得する関数
         緯度経度を指定した場合は、その地点から近い図書館を順に出力します。
@@ -32,9 +33,9 @@ class Pycalil(object):
 
         geocode: str
             緯度、経度を指定します。
-        
+
         format: str
-            出力形式を指定します。xmlまたはjsonです。
+            出力形式を指定します。xmlまたはjsonです。デフォルトはxml
         
         callback: str
             JSONPのcallback関数名を指定します。
@@ -50,20 +51,20 @@ class Pycalil(object):
         # parameters
         payload = {
             "appkey": self.apikey,
-            "pref" : pref,
-            "city" : city,
-            "systemid" : systemid,
-            "geocode" : geocode,
-            "format" : format,
-            "callback" : callback,
-            "limit" : limit,
+            "pref": pref,
+            "city": city,
+            "systemid": systemid,
+            "geocode": geocode,
+            "format": format,
+            "callback": callback,
+            "limit": limit,
         }
 
         # getリクエスト送信
         r = requests.get(library_url, params=payload)
-        return r.text
+        return r.json()
 
-    def check(self, isbn, systemid, format="json", callback="json"):
+    def check(self, isbn, systemid, format="json", callback="no"):
         """
         図書館に対して蔵書の有無と貸出状況を問い合わせます。
 
@@ -78,8 +79,7 @@ class Pycalil(object):
             カンマ区切りで複数指定できます。
 
         format : str
-            出力形式を指定します。
-            xmlまたはjsonです。デフォルトはjson
+            出力形式を指定します。xmlまたはjsonです。デフォルトはjson
 
         callback : str
             JSONPのcallback関数名を指定します。
@@ -91,12 +91,12 @@ class Pycalil(object):
 
         # parameters
         payload = {
-            "appkey" : self.apikey,
-            "isbn" : ",".join(map(str, isbn)),
-            "systemid" : ",".join(systemid),
-            "format" : format,
-            "callback" : callback,
+            "appkey": self.apikey,
+            "isbn": ",".join(map(str, isbn)),
+            "systemid": ",".join(systemid),
+            "format": format,
+            "callback": callback,
         }
         
         r = requests.get(check_url, params=payload)
-        return r.text
+        return r.json()
